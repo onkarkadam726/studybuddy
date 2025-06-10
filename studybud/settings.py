a51a2b6cc564c083f826688e7ca471e6e53a97c6
Onkar Kadam <onkarok619@gmail.com>
2025-06-10 22:14:29 +0530

    imported os

--- studybud/settings.py
+++ studybud/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from django.conf.global_settings import AUTH_USER_MODEL, MEDIA_ROOT
