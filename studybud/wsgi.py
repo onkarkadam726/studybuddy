1aa1512fc1197a175d6dcea2a3bc5b7b1f072b7b
Onkar Kadam <onkarok619@gmail.com>
2025-06-16 02:45:28 +0530

    Add automatic superuser creation

--- studybud/wsgi.py
+++ studybud/wsgi.py
@@ -16,3 +16,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studybud.settings')
 application = get_wsgi_application()
 
 
+#You said:
+#import django
+#from django.core.management import call_command
+
+#django.setup()
+#try:
+#    call_command('makemigrations', interactive=False)
+#    call_command('migrate', interactive=False)
+#except Exception as e:
+#    print(f"Migration failed: {e}")
