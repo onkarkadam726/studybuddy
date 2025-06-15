869d7081089d955c9dad5208f25e4a8febd7af92
Onkar Kadam <onkarok619@gmail.com>
2025-06-16 03:16:22 +0530

    removed superuser

--- base/urls.py
+++ base/urls.py
@@ -24,6 +24,3 @@ urlpatterns = [
 ]
 
 
-from django.contrib.auth.models import User
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')
