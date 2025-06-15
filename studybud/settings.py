5d85b59e13ba72861391aff79a9d3c0f203c20b1
Onkar Kadam <onkarok619@gmail.com>
2025-06-15 17:08:28 +0530

    Add dynamic DB config: SQLite for local, PostgreSQL for Render

--- studybud/settings.py
+++ studybud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 from django.conf.global_settings import AUTH_USER_MODEL, MEDIA_ROOT
 
@@ -88,12 +89,19 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv("RENDER") == "true":
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL')
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
