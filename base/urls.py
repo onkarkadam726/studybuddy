702ef6e7762aae1d637774f67dea6e4a3016a037
Onkar Kadam <onkarok619@gmail.com>
2025-06-16 03:09:41 +0530

    Add automatic superuser creation

--- base/urls.py
+++ base/urls.py
@@ -22,3 +22,8 @@ urlpatterns = [
     path('activity/', views.activityPage, name="activity"),
 
 ]
+
+
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')
